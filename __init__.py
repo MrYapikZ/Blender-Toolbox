bd94c7c5199ae3f78b1a4a4e706f8ea727e494a5
MrYapikZ <yafialhaidar18@gmail.com>
2025-10-28 10:48:10 +0700

    release: v0.1.10

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "MXTools",
     "description": "Tools for Blender",
     "author": "MrYapikZ",
-    "version": (0, 1, 9),
+    "version": (0, 1, 10),
     "blender": (4, 5, 0),
 }
 
