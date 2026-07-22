1a7707d37f0af5d57111a52670ef5621fd5cee7b
MrYapikZ <yafialhaidar18@gmail.com>
2026-07-22 13:30:57 +0700

    release: v0.2.14

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "DelthλTools",
     "description": "Tools for Blender",
     "author": "Yapi",
-    "version": (0, 2, 13),
+    "version": (0, 2, 14),
     "blender": (4, 5, 0),
 }
 
